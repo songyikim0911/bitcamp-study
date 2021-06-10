b548af57ccd5220492db5f65b7a1677cdcf734ec
songyikim0911 <ksi6470@gmail.com>
2021-06-10 10:12:59 +0900

    dele

--- hello2.js
+++
@@ -1,6 +0,0 @@
-console.log("A개발자");
-console.log("Hello, world!");
-console.log("B개발자")
-console.log("BB개발자0")
-var studentName = "홍길동";
-console.log("이름: "+ studentName);
--- hello3.js
+++
@@ -1,6 +0,0 @@
-
-console.log("Hello, B개발자. upside");
-
-console.log("Hello, world!");
-
-console.log("B developer, bottom");
